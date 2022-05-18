dc45e9c55f6bb4c03abeac47a8808debb4aa87f8
EZBreezy98 <104875936+EZBreezy98@users.noreply.github.com>
2022-05-18 14:02:15 +1000

    Update main.cpp
    added second paddle for 2 players.
    added placeholder text for scoring system
    player's cannot exit boundary of the window height.
    added some comments explaining existing and new code

--- Pong CPP/RaylibStarterCPP/main.cpp
+++ Pong CPP/RaylibStarterCPP/main.cpp
@@ -22,6 +22,16 @@ int main(int argc, char* argv[])
 
     float p1Speed = 1000;
 
+    //Player2 Variable
+    float p2x = 700;
+    float p2y = 150;
+    //I know there is no reason to keep this in memory because we already have the value from p1 but it's less confusing to read this way for a larger project
+    // I'd care but for something small like this it doesn't change much
+    float p2Width = 20;
+    float p2Height = 125;
+
+    float p2Speed = 1000;
+
     SetTargetFPS(60);
     //--------------------------------------------------------------------------------------
 
@@ -30,24 +40,57 @@ int main(int argc, char* argv[])
     {
         // Update
         //----------------------------------------------------------------------------------
+
+        /*player one movement : if player is holding W then decrease Y coordinate to make the player 1 paddle go up (this is because the window is essentially in
+        the bottom right quadrant of a number plane thus in it's current position decreasing the y value makes the paddle go up)*/ 
         if (IsKeyDown(KEY_W))
         {
             p1y = p1y + -1 * p1Speed * deltatime;
+            if (p1y < 0)
+            {
+                p1y = 0;
+            }
         }
-
         if (IsKeyDown(KEY_S))
         {
             p1y = p1y + 1 * p1Speed * deltatime;
+            if (p1y > 325)
+            {
+                p1y = 325;
+            }
         }
         
+        if (IsKeyDown(KEY_UP))
+        {
+            p2y = p2y + -1 * p2Speed * deltatime;
+            if (p2y < 0)
+            {
+                p2y = 0;
+            }
+        }
+        if (IsKeyDown(KEY_DOWN))
+        {
+            p2y = p2y + 1 * p1Speed * deltatime;
+            if (p2y > 325)
+            {
+                p2y = 325;
+            }
+        }
+
         // Draw
         //----------------------------------------------------------------------------------
         counter++;
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
+        //draws player 1 paddle
         DrawRectangle(p1x, p1y, p1Width, p1Height, BLACK);
-        DrawText("counterText", 190, 200, 20, LIGHTGRAY);
+        //draws player 2 paddle
+        DrawRectangle(p2x, p2y, p1Width, p1Height, BLACK);
+        //draws score text
+        DrawText("0", 350, 20, 80, LIGHTGRAY);
+        DrawText("|", 410, 20, 80, LIGHTGRAY);
+        DrawText("0", 440, 20, 80, LIGHTGRAY);
         
 
         EndDrawing();
