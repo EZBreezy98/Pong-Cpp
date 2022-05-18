f758a0a1ba156f004120abb75cc4cbb6f468d0e4
EZBreezy98 <104875936+EZBreezy98@users.noreply.github.com>
2022-05-18 12:37:44 +1000

    Updated main.cpp
    Updated main.cpp to include new variables to keep track of player 1 paddle position and width/height. Update now also checks for key presses to move the paddle

--- Pong CPP/RaylibStarterCPP/main.cpp
+++ Pong CPP/RaylibStarterCPP/main.cpp
@@ -1,26 +1,4 @@
-/*******************************************************************************************
-*
-*   raylib [core] example - Basic window
-*
-*   Welcome to raylib!
-*
-*   To test examples, just press F6 and execute raylib_compile_execute script
-*   Note that compiled executable is placed in the same folder as .c file
-*
-*   You can find all basic examples on C:\raylib\raylib\examples folder or
-*   raylib official webpage: www.raylib.com
-*
-*   Enjoy using raylib. :)
-*
-*   This example has been created using raylib 1.0 (www.raylib.com)
-*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
-*
-*   Copyright (c) 2014 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
-
 #include "raylib.h"
-
 #define RAYGUI_IMPLEMENTATION
 #define RAYGUI_SUPPORT_ICONS
 #include "raygui.h"
@@ -32,7 +10,17 @@ int main(int argc, char* argv[])
     int screenWidth = 800;
     int screenHeight = 450;
 
+    int counter = 1;
+    float deltatime = 0.005f;
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+    //Player1 variable declarations
+    float p1x = 100;
+    float p1y = 150;
+    float p1Width = 20;
+    float p1Height = 125;
+
+    float p1Speed = 1000;
 
     SetTargetFPS(60);
     //--------------------------------------------------------------------------------------
@@ -42,16 +30,25 @@ int main(int argc, char* argv[])
     {
         // Update
         //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
+        if (IsKeyDown(KEY_W))
+        {
+            p1y = p1y + -1 * p1Speed * deltatime;
+        }
 
+        if (IsKeyDown(KEY_S))
+        {
+            p1y = p1y + 1 * p1Speed * deltatime;
+        }
+        
         // Draw
         //----------------------------------------------------------------------------------
+        counter++;
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
-
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        DrawRectangle(p1x, p1y, p1Width, p1Height, BLACK);
+        DrawText("counterText", 190, 200, 20, LIGHTGRAY);
+        
 
         EndDrawing();
         //----------------------------------------------------------------------------------
